21c119e05c5aa933e4d29676c3a09ce12e6dbb85
mirandakop <mkoppejan@145-24-227-37.wlan.hro.nl>
2015-01-22 15:10:48 +0100

    Ontwerp kookplaat
    Werkend

--- myFirstApp/myFirstApp.js
+++ myFirstApp/myFirstApp.js
@@ -43,10 +43,9 @@ if (Meteor.isClient) {
   });
 
 
-
   // Template.hello.events({
   //   'click button': function () {
-  //     // increment the counter when button is clicked
+  //     // increment the counter when butto is clicked
   //     Session.set("counter", Session.get("counter") + 1);
   //   }
   // });

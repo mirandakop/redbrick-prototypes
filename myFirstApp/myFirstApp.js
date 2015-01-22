90b832f6c4927f085e5994889bb246de6c78c0b8
mirandakop <mkoppejan@145-24-227-37.wlan.hro.nl>
2015-01-22 16:47:25 +0100

    <template name="leaderboard">   <div class="leaderboard">     {{#each players}}     {{> player}}     {{/each}}   </div>   {{#if selected_name}}   <div class="details">     <div class="name">{{selected_name}}</div>     <input type="button" class="inc" value="Give 5 points" />   </div>   {{/if}}   {{#unless selected_name}}   <div class="none">Click a player to select</div>   {{/unless}} </template> <template name="player">   <div class="player {{selected}}">     <span class="name">{{name}}</span>     <span class="score">{{score}}</span>   </div> </template>

--- myFirstApp/myFirstApp.js
+++ myFirstApp/myFirstApp.js
@@ -49,7 +49,31 @@ if (Meteor.isClient) {
   //     Session.set("counter", Session.get("counter") + 1);
   //   }
   // });
+  //  Template.leaderboard.players = function () {
+  //    return Players.find({}, {sort: {score: -1, name: 1}});
+  //};
+  //Template.leaderboard.selected_name = function () {
+  //    var player = Players.findOne(Session.get("selected_player"));
+  //    return player && player.name;
+  //  };
+  //  Template.player.selected = function () {
+  //    return Session.equals("selected_player", this._id) ? "selected" : '';
+  //};
+  //  Template.leaderboard.events({
+  //    'click input.inc': function () {
+  //      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+  //    }
+  //  });
+  //  Template.player.events({
+  //    'click': function () {
+  //      Session.set("selected_player", this._id);
+  //    }
+  //  });
+
 }
+
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -92,6 +116,9 @@ if (Meteor.isServer) {
 
     });
 
+
+
+
     Meteor.setInterval(function(){
       messageCollection.update({_id:messageID},
 		  {
@@ -104,5 +131,6 @@ if (Meteor.isServer) {
     },50);
 
   });
+
 
 }
